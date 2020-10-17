c5bd7fe37b8b6843814e81a7d830bde9e8f7cae5
Yeoh Wong <eyeoh@usc.edu>
2020-10-17 09:40:14 -0700

    Added UI_Assistant and TextWriter
    Written functions to display text as "being written on screen".
    
    Bug: When running game, text box does not shows up, but function is being ran.

---
+++ Assets/Scripts/TextWriter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWriter : MonoBehaviour
+{
+
+    private Text uiText;
+    private string textMessage;
+    private float timePerChar;
+    private float timer;
+    private int textIndex;
+    public void AddWriter(Text uiText, string textMessage, float timePerChar)
+    {
+        this.uiText = uiText;
+        this.textMessage = textMessage;
+        this.timePerChar = timePerChar;
+        this.textIndex = 0;
+    }
+
+    private void Update()
+    {
+
+        if (uiText != null)
+        {
+            // decrement the timer to move on to the next character
+            timer -= Time.deltaTime;
+            if (timer <= 0.0f )
+            {
+                // display the next character in the string 
+                timer += timePerChar;
+                textIndex++;
+                uiText.text = textMessage.Substring(0, textIndex);
+
+                // return if we've ran out of characters to display
+                if (textIndex >= textMessage.Length)
+                {
+                    uiText = null;
+                    return;
+                }
+            }
+        }
+
+
+    }
+
+}
---
+++ Assets/Scripts/UI_Assistant.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Assistant : MonoBehaviour
+{
+    [SerializeField] private TextWriter textWriter;
+    private Text messageText;
+
+    private void Awake() {
+
+        messageText = transform.Find("Message").Find("messageText").GetComponent<Text>();
+    }
+
+    private void Start()
+    {
+        textWriter.AddWriter(messageText, "Display Message Here", 0.2f);
+    }
+}

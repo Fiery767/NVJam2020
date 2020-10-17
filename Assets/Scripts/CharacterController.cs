5e4b687518d765aeb4b2debbbdf0472d06b80479
dexterknaack <dexter.knaack@gmail.com>
2020-10-16 20:56:19 -0700

    Started Character Controller, movement setup (no state)
    10/16/20 8:56

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    public float jumpStrength;
+    enum moveState
+    {
+        jumping,
+        running,
+        idle
+    };
+    moveState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = moveState.idle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state != moveState.jumping && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0, jumpStrength));
+            state = moveState.jumping;
+        }
+        if (state != moveState.jumping && Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(new Vector2(0, jumpStrength));
+            state = moveState.jumping;
+        }
+        if (state != moveState.jumping && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.AddForce(new Vector2(0, jumpStrength));
+            state = moveState.jumping;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.tag == "ground")
+        //{
+        //
+        //}
+    }
+
+
+}

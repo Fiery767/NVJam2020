d8aacc9aeb4138ef1cbdaf7a0699fd6a91a82ad5
dexterknaack <dexter.knaack@gmail.com>
2020-10-18 13:13:09 -0700

    Animations (ugh)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,6 +17,9 @@ public class PlayerController : MonoBehaviour
     private float WallingGravity = 0.75f;
     private float JumpDirX = 0;
     private bool grounded = false;
+    private Animator anim;
+    private bool facing;
+    private SpriteRenderer sr;
     
 
     enum MoveState
@@ -32,11 +35,16 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        facing = true;
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
         m_state = MoveState.idle;
     }
 
     private void Update()
     {
+        if(facing) { sr.flipX = false; }
+        else { sr.flipX = true; }
         if(Mathf.Abs(rb.velocity.x)>MaxVel)
         {
             rb.velocity = new Vector2((Mathf.Abs(rb.velocity.x)/rb.velocity.x) * MaxVel, rb.velocity.y);
@@ -66,24 +74,40 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        //Debug.Log(m_state);
+        bool isActuallyGrounded = grounded && !(m_state == MoveState.walling) && !(m_state == MoveState.jumping);
+        Debug.Log(m_state);
         //Debug.Log(grounded);
         if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && !grounded)
         {
             rb.velocity = new Vector2(rb.velocity.x - velSpeed * Time.fixedDeltaTime, rb.velocity.y);
+            facing = false;
         }
         else if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && grounded)
         {
             transform.position = new Vector3(transform.position.x - speed * Time.fixedDeltaTime, transform.position.y, transform.position.z);
-
+            facing = false;
+            if (isActuallyGrounded)
+            {
+                SetState(MoveState.running);
+            }
         }
         else if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && !grounded)
         {
+            facing = true;
             rb.velocity = new Vector2(rb.velocity.x + velSpeed * Time.fixedDeltaTime, rb.velocity.y);
         }
         else if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && grounded)
         {
+            facing = true;
             transform.position = new Vector3(transform.position.x + speed * Time.fixedDeltaTime, transform.position.y, transform.position.z);
+            if (isActuallyGrounded)
+            {
+                SetState(MoveState.running);
+            }
+        }
+        else if (grounded && m_state == MoveState.running)
+        {
+            SetState(MoveState.idle);
         }
     }
 
@@ -101,9 +125,28 @@ public class PlayerController : MonoBehaviour
         switch (state)
         {
             case MoveState.jumping:
+                anim.SetBool("Idle", false);
+                anim.SetBool("Wall", false);
+                anim.SetBool("Run", false);
+                anim.SetBool("Jump", true);
                 break;
             case MoveState.walling:
-                Debug.Log("WE ARE WALL");
+                anim.SetBool("Idle", false);
+                anim.SetBool("Run", false);
+                anim.SetBool("Jump", false);
+                anim.SetBool("Wall", true);
+                break;
+            case MoveState.running:
+                anim.SetBool("Idle", false);
+                anim.SetBool("Jump", false);
+                anim.SetBool("Wall", false);
+                anim.SetBool("Run", true);
+                break;
+            case MoveState.idle:
+                anim.SetBool("Run", false);
+                anim.SetBool("Jump", false);
+                anim.SetBool("Wall", false);
+                anim.SetBool("Idle", true);
                 break;
         }
 
@@ -164,8 +207,8 @@ public class PlayerController : MonoBehaviour
         if (m_state != MoveState.idle)
         {
             SetState(MoveState.walling);
-            if (side) { JumpDirX = 1; }
-            else { JumpDirX = -1; }
+            if (side) { JumpDirX = 1; facing = true; }
+            else { JumpDirX = -1; facing = false; }
         }
     }
 }
